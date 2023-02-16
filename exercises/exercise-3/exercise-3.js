514a58fc8d45800957a0d290e792a84eae7f8e49
elahemortazavi <emor5680@gmail.com>
2023-02-16 00:04:25 +0000

    Update exercise-3.js

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -1,3 +1,9 @@
+
+// you have been provided with a takeout order. Write a program that will print out the receipt for this order.
+// - Log each individual item to the console.
+// - Log the total cost of the order to the console.
+
+
 let order = [
   { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29 },
   { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39 },
